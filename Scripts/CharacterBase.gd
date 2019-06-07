b2dbcf22f36404dfaeb8b3e6f2bd46b6be01c7dc
Suecra <31817546+Suecra@users.noreply.github.com>
2019-06-07 20:19:52 +0200

    Maps and stuff
    - new maps
    - player visibility indicator
    - debug buttons
    - initial direction of characters

--- Scripts/CharacterBase.gd
+++ Scripts/CharacterBase.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 export(int) var hp = 5
 export(int) var movement_speed = 60
 export(float) var special_cooldown = 4.0
+export(Vector2) var initial_direction = Vector2(0, 1)
 
 var movement = Vector2(0, 0)
 var direction = Vector2(0, 1)
@@ -10,6 +11,7 @@ var attacking = false
 var current_hp
 var speed_multiplier = 1
 var special_ready = true
+var visibility = 0.0
 
 func move(direction: Vector2):
 	if direction != Vector2(0, 0):
@@ -51,6 +53,7 @@ func _faint():
 
 func _physics_process(delta):
 	move_and_slide(movement * speed_multiplier)
+	update_visibility()
 	_update_graphics()
 	movement = Vector2(0, 0)
 	attacking = false
@@ -62,8 +65,32 @@ func _update_graphics():
 	$BlackSprite.moving = movement != Vector2(0, 0)
 	$BlackSprite.direction = direction
 
+func update_visibility():
+	var tiles = get_tiles_on_current_position()
+	if tiles[1] == 4:
+		visibility = 0
+	else:
+		if tiles[0] == 0:
+			if tiles[1] == -1:
+				visibility = 0
+			else:
+				visibility = 0.5
+			
+		else:
+			visibility = 1.0
+
+func get_tiles_on_current_position():
+	var layer1 = get_parent().get_node("BBCopy1/Layer1")
+	var layer2 = get_parent().get_node("BBCopy2/Layer2")
+	var tile_v = position / 16
+	var result = []
+	result.append(layer1.get_cellv(tile_v))
+	result.append(layer2.get_cellv(tile_v))
+	return result
+
 func _ready():
 	current_hp = hp
+	direction = initial_direction
 	set_physics_process(true)
 	$WhiteSprite.direction = direction
 	$BlackSprite.direction = direction
--- Scripts/PlayerMagician.gd
+++ Scripts/PlayerMagician.gd
@@ -1,7 +1,10 @@
 extends "res://Scripts/Magician.gd"
 
+var old_movement_speed: int
+
 func _update_graphics():
 	._update_graphics()
+	print(visibility)
 	$Silhouette.moving = movement != Vector2(0, 0)
 	$Silhouette.direction = direction
 
@@ -13,10 +16,13 @@ func _physics_process(delta):
 
 func _do_special_move(direction: Vector2):
 	._do_special_move(direction)
+	old_movement_speed = movement_speed
+	movement_speed = 100
 	$Teleporter.position = teleport_position
 	$Teleporter/AnimationPlayer.play("teleport_animation")
 	$Teleporter.visible = true
 
 func _stop_special_move():
 	._stop_special_move()
+	movement_speed = old_movement_speed
 	$Teleporter.visible = false
