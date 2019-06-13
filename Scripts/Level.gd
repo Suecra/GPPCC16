fdb8c34b2bf40529148b25834eedcee48e0f2865
Suecra <31817546+Suecra@users.noreply.github.com>
2019-06-13 20:23:24 +0200

    Final adjustments
    - More teleporters added
    - Music in every stage
    - Debug keys removed

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -1,41 +1,4 @@
 extends Node2D
 
-const SWORD_FIGHTER = 0
-const MAGICIAN = 1
-const MARKSMAN = 2
-
 export(int) var width
 export(int) var height
-
-var player_class = -1
-
-func _physics_process(delta):
-	do_debug_keys()
-
-func do_debug_keys():
-	if Input.is_key_pressed(KEY_1):
-		goto_stage("Level1")
-	if Input.is_key_pressed(KEY_2):
-		goto_stage("Level2")
-	if Input.is_key_pressed(KEY_3):
-		goto_stage("Level3")
-	if Input.is_key_pressed(KEY_4):
-		goto_stage("Level4")
-	if Input.is_key_pressed(KEY_5):
-		goto_stage("Level5")
-	if Input.is_key_pressed(KEY_6):
-		goto_stage("Level6")
-	if Input.is_key_pressed(KEY_7):
-		goto_stage("TestArea")
-	if Input.is_key_pressed(KEY_F1):
-		change_player(SWORD_FIGHTER)
-	if Input.is_key_pressed(KEY_F2):
-		change_player(MAGICIAN)
-	if Input.is_key_pressed(KEY_F3):
-		change_player(MARKSMAN)
-
-func goto_stage(name: String):
-	get_tree().change_scene("res://Scenes/Levels/" + name + ".tscn")
-
-func change_player(fighter_class):
-	$Player.transform_to(fighter_class)
